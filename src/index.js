4fe24282d145b108b5160a5aa23da889f1f801ad
github username <github email address>
2018-02-28 09:33:29 -0500

    add redux store, actions, and reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import peopleReducer from './reducers/peopleReducer'
 
+const store = createStore(peopleReducer,  /* preloadedState, */ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
