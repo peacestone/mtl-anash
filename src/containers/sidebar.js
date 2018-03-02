d9d768598a8ec07f60428e7521d63001db02145d
github username <github email address>
2018-03-01 21:30:24 -0500

    add componentDidMount requests for people

--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -3,18 +3,24 @@ import Listings from '../components/listings'
 import SearchInput from '../components/searchInput'
 import Fuse from 'fuse.js'
 import {Segment, Container} from 'semantic-ui-react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import {fetchPeople} from '../actions/peopleActions'
 
 class Sidebar extends Component {
 
   constructor(props) {
     super(props)
-    this.people = [{firstName: 'roger', lastName: 'albertson'}, {firstName: 'Tim', lastName: 'wahsington'}, {firstName: "chani", lastName: 'steinmetz'} ]
+    this.people = []
     this.state = {
       searchInput: '',
-      results: this.people
+      results: []
     }
 
   }
+
+  componentDidMount = () => this.props.fetchPeople()
+
 
   handleSearchInput = (event, test) => { 
     this.setState({searchInput: event.target.value}, (props) => {
@@ -43,8 +49,11 @@ class Sidebar extends Component {
         <SearchInput handleSearchInput={this.handleSearchInput} searchValue={this.state.searchInput}  />
         <Listings people={this.state.results} isEmptySearchInput={this.state.searchInput === ''} />
     </Segment>
-  )
-}
+  )}
 }
 
-export default Sidebar
+ const mapDispatchToProps = dispatch => (
+   bindActionCreators({fetchPeople}, dispatch)
+ )
+
+export default connect(null, mapDispatchToProps)(Sidebar)
