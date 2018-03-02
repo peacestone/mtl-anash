1e671e4d66ae9479f99f03eec2e9dbc4ef26777c
github username <github email address>
2018-03-02 10:10:09 -0500

    add people list to props

--- src/actions/peopleActions.js
+++ src/actions/peopleActions.js
@@ -1,11 +1,8 @@
  
     const fetchPeople = (people) => {
-        return {type: "GET_PEOPLE", payload:  [{firstName: 'roger', lastName: 'albertson'}, {firstName: 'Tim', lastName: 'wahsington'}, {firstName: "chani", lastName: 'steinmetz'}]  }
+        return {type: "FETCH_PEOPLE", payload:  [{firstName: 'roger', lastName: 'albertson'}, {firstName: 'Tim', lastName: 'wahsington'}, {firstName: "chani", lastName: 'steinmetz'}]  }
     }
 
-    const addQueryInput = (query) => {
-        return {type: 'ADD_QUERY_INPUT', payload: query}
-    }
 
     export {fetchPeople}
 
